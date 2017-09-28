09ef5126f1011ff2338afa49afbddbc3b7300184
Ali Hashim <ahashim@u.rochester.edu>
2017-09-27 23:33:01 -0400

    Added index php file

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Portfolios</title>
+
+</head>
+
+<body>
+
+	<h1>Portfolios</h1>
+
+	<section>
+		<h2>John Denzer</h2>
+		<p>John studies financial economics at the University of Rochester. John has an avid interest in sports and computers. He enjoys following tech news and one of his favorite companies is AMD. John is a right handed starting pitcher and was 2nd in the NCAA for least amount of walks per nine innings.</p>
+		<a href="johndenzer.php">Read more about John</a>
+
+	</section>
+
+	<section>
+		<h2>Ali Hashim</h2>
+
+		<p>Ali is a twenty-one year old optical engineer studying at the University of Rochester. He is passionate about manipulating light to create great products. He is also a self-taught mobile application developer and graphic designer and is looking to apply his skills to serve a design, testing, and manufacturing role.</p>
+
+		<a href="alihashim.php">Read more about Ali</a>
+
+	</section>
+
+	<section>
+		<h2>Aaron Raymond</h2>
+
+		<p>Aaron is currently a senior studying Environmental Studies at the University of Rochester. He enjoys combining his photographic skills with his computer skills to create stunning websites and user experiences. He also has experience in the IT world which has helped strengthen his communication and problem solving skills.</p>
+
+		<a href="alihashim.php">Read more about Aaron</a>
+
+	</section>
+
+</body>
+</html>
