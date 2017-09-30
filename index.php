f520affa086237c9ccad995b9e315c8fd153aa46
aaraymond <aaraymond@me.com>
2017-09-29 23:05:14 -0400

    removed projection
    html checker says projection is antiquated and stackoverflow says its only useful for older versions of opera

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
 	<!-- Importing fonts -->
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<!-- Importing materialize css (minimized) -->
-	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen"/>
 	<!-- Importing override file -->
-	<link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+	<link href="css/style.css" type="text/css" rel="stylesheet" media="screen"/>
 	<!-- Setting viewport -->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<meta charset="utf-8">
