9f4c5c46d660227b6a05ae79c6cb51bdc82ceb1e
aaraymond <aaraymond@me.com>
2017-10-02 11:44:40 -0400

    moblie friendly grid
    changed grid display preferences for smaller screen widths.

--- index.php
+++ index.php
@@ -38,38 +38,38 @@
 	<div class="container">
 
 	<div class="row"><!-- row 1 -->
-		<div class="card-panel blue-grey lighten-2 z-depth-0 col s8 push-s4">
+		<div class="card-panel blue-grey lighten-2 z-depth-0 col m12 l8 push-l4">
 			<h2>John Denzer</h2>
 			<p>John studies financial economics at the University of Rochester. John has an avid interest in sports and computers. He enjoys following tech news and one of his favorite companies is AMD. John is a right handed starting pitcher and was 2nd in the NCAA for least amount of walks per nine innings.</p>
 			<a class="btn light-blue lighten-2 z-depth-0" href="johndenzer.php">Read more about John</a>
 		</div>
-		<div class="col s4 pull-s8 center-align">
+		<div class="col l4 pull-l8 center-align">
 			<img src="images/baseball.png" alt="Baseball player hitting a ball" height="200" width="200">
 		</div>
 	</div><!-- row 1 -->
 
 	<div class="row"><!-- row 2 -->
-		<div class="card-panel blue-grey lighten-2 z-depth-0 col s8 ">
+		<div class="card-panel blue-grey lighten-2 z-depth-0 col m12 l8 ">
 			<h2>Ali Hashim</h2>
 
 			<p>Ali is a twenty-one year old optical engineer studying at the University of Rochester. He is passionate about manipulating light to create great products. He is also a self-taught mobile application developer and graphic designer and is looking to apply his skills to serve a design, testing, and manufacturing role.</p>
 
 			<a class="btn light-blue lighten-2 z-depth-0" href="alihashim.php">Read more about Ali</a>
 		</div>
-		<div class="col s4 center-align">
+		<div class="col l4 center-align">
 			<img src="images/laptop_icon.png" alt="Laptop icon" height="200" width="200">
 		</div>
 	</div><!-- row 2 -->
 
 	<div class="row"><!-- row 3 -->
-		<div class="card-panel blue-grey lighten-2 z-depth-0 col s8 push-s4">
+		<div class="card-panel blue-grey lighten-2 z-depth-0 col m12 l8 push-l4">
 			<h2>Aaron Raymond</h2>
 
 			<p>Aaron is currently a senior studying Environmental Studies at the University of Rochester. He enjoys combining his photographic skills with his computer skills to create stunning websites and user experiences. He also has experience in the IT world which has helped strengthen his communication and problem solving skills.</p>
 
 			<a class="btn light-blue lighten-2 z-depth-0" href="alihashim.php">Read more about Aaron</a>
 		</div>
-		<div class="col s4 pull-s8 center-align">
+		<div class="col l4 pull-l8 center-align">
 			<img src="images/camera.png" alt="Camera Icon" height="200" width="200">
 		</div>
 	</div><!-- row 3 -->
