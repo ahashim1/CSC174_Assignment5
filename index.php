8d7eeeadff50d4bf3e6b68e7c74fd283866dd3ba
aaraymond <aaraymond@me.com>
2017-10-02 11:33:59 -0400

    homepage grid
    added css grid through materialize classes. still need to figure out padding for bottom of each "card-panel" class. modified some css that was used to try to get the a:hover and a:active attributes to work. added a different laptop icon to match pixel size of other icons.

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 <body>
 		
 		<!-- navigation done by John -->
-		<nav class="z-depth-0">
+		<nav class="z-depth-0 blue-grey lighten-2">
 			<div class="nav-container nav-wrapper">
 				<a href="index.php" class="home-link-left">Home</a>
 				<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i>>
@@ -37,32 +37,44 @@
 
 	<div class="container">
 
-	<section>
-		<h2>John Denzer</h2>
-		<p>John studies financial economics at the University of Rochester. John has an avid interest in sports and computers. He enjoys following tech news and one of his favorite companies is AMD. John is a right handed starting pitcher and was 2nd in the NCAA for least amount of walks per nine innings.</p>
-		<a class="btn light-blue lighten-2" href="johndenzer.php">Read more about John</a>
+	<div class="row"><!-- row 1 -->
+		<div class="card-panel blue-grey lighten-2 z-depth-0 col s8 push-s4">
+			<h2>John Denzer</h2>
+			<p>John studies financial economics at the University of Rochester. John has an avid interest in sports and computers. He enjoys following tech news and one of his favorite companies is AMD. John is a right handed starting pitcher and was 2nd in the NCAA for least amount of walks per nine innings.</p>
+			<a class="btn light-blue lighten-2 z-depth-0" href="johndenzer.php">Read more about John</a>
+		</div>
+		<div class="col s4 pull-s8 center-align">
+			<img src="images/baseball.png" alt="Baseball player hitting a ball" height="200" width="200">
+		</div>
+	</div><!-- row 1 -->
 
-	</section>
+	<div class="row"><!-- row 2 -->
+		<div class="card-panel blue-grey lighten-2 z-depth-0 col s8 ">
+			<h2>Ali Hashim</h2>
 
-	<section>
-		<h2>Ali Hashim</h2>
+			<p>Ali is a twenty-one year old optical engineer studying at the University of Rochester. He is passionate about manipulating light to create great products. He is also a self-taught mobile application developer and graphic designer and is looking to apply his skills to serve a design, testing, and manufacturing role.</p>
 
-		<p>Ali is a twenty-one year old optical engineer studying at the University of Rochester. He is passionate about manipulating light to create great products. He is also a self-taught mobile application developer and graphic designer and is looking to apply his skills to serve a design, testing, and manufacturing role.</p>
+			<a class="btn light-blue lighten-2 z-depth-0" href="alihashim.php">Read more about Ali</a>
+		</div>
+		<div class="col s4 center-align">
+			<img src="images/laptop_icon.png" alt="Laptop icon" height="200" width="200">
+		</div>
+	</div><!-- row 2 -->
 
-		<a class="btn" href="alihashim.php">Read more about Ali</a>
+	<div class="row"><!-- row 3 -->
+		<div class="card-panel blue-grey lighten-2 z-depth-0 col s8 push-s4">
+			<h2>Aaron Raymond</h2>
 
-	</section>
+			<p>Aaron is currently a senior studying Environmental Studies at the University of Rochester. He enjoys combining his photographic skills with his computer skills to create stunning websites and user experiences. He also has experience in the IT world which has helped strengthen his communication and problem solving skills.</p>
 
-	<section>
-		<h2>Aaron Raymond</h2>
-
-		<p>Aaron is currently a senior studying Environmental Studies at the University of Rochester. He enjoys combining his photographic skills with his computer skills to create stunning websites and user experiences. He also has experience in the IT world which has helped strengthen his communication and problem solving skills.</p>
-
-		<a class="btn" href="alihashim.php">Read more about Aaron</a>
-
-	</section>
+			<a class="btn light-blue lighten-2 z-depth-0" href="alihashim.php">Read more about Aaron</a>
+		</div>
+		<div class="col s4 pull-s8 center-align">
+			<img src="images/camera.png" alt="Camera Icon" height="200" width="200">
+		</div>
+	</div><!-- row 3 -->
 	
-	</div>
+	</div><!-- container -->
 	
 	<!-- Init JQ and collapse menu-->
 	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
