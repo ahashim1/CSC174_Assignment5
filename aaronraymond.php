1f16de034f71dcc09af71df131c58bfaf2a8d812
aaraymond <aaraymond@me.com>
2017-10-04 20:29:03 -0400

    aaron grid
    updated aaron portfolio grid to match ali and john

--- aaronraymond.php
+++ aaronraymond.php
@@ -7,13 +7,18 @@
 	 <?php include "inc/navigation.inc";?>
 
 	<div class="container">
-
-		<div class="row"><!-- row 1 -->
-			<div class="col s12 m8 card-panel blue-grey lighten-2 z-depth-0">
+		<div class="wrapper">
+			<div class="one"><!-- one -->
 				<header>
-					<h1 class="center-align aaron">Aaron Raymond</h1>
+					<h1 class="center-align">Aaron Raymond</h1>
 				</header>
+			</div>
 
+			<div class="two">
+				<img class="responsive-img" src="images/aaron.jpg" alt="Aaron Raymond">
+			</div>
+
+			<div class="three card-panel blue-grey lighten-2 z-depth-0">
 				<!-- About Me Section -->
 				<section>
 					<h2 class="portfolio">About Me</h2>
@@ -22,15 +27,8 @@
 				</section>
 			</div>
 
-			<div class="col s12 m4">
-				<img class="responsive-img" src="images/aaron.jpg" alt="Aaron Raymond">
-			</div>
-
-		</div><!-- row 1 -->
-
-		<div class="row"><!-- row 2 -->
-			<!-- left column -->
-			<div class="col s12 m8 card-panel blue-grey lighten-2 z-depth-0">
+			<!-- four -->
+			<div class="four card-panel blue-grey lighten-2 z-depth-0">
 				<!-- Main Experience -->
 				<main>
 					<h2 class="portfolio">Experience</h2>
@@ -67,23 +65,25 @@
 						<p>Working at DCS expanded my knowledge in communications and problem solving through the installation and management of hundreds of computers. Managing the computer network for a large commercial real estate company was my primary task and in doing so I gained valuable experience in the corporate setting including setting up and ensuring conference rooms were prepared in advance of meetings and all computers were connected and fully prepared for presentations. My talents also included managing customer’s Macs in conjunction with Windows Small Business Server.</p>
 					</section>
 				</main>	
-			</div><!-- left column -->
-		
-			<div class="s12 m4 card-panel blue-grey lighten-2 z-depth-0">
-				<!-- My Extracurriculars -->
-				<section>
-					<h2 class="portfolio">Extracurriculars</h2>
-						
-						<p>I founded the UR Photography Club in 2015 to promote the learning of, creating, and sharing of the art of photography. Through my leadership of the Photo Club, I have transformed the photography community at the University of Rochester into a collaborative organization providing photography services to the campus community. In addition to professional services, Photo Club also holds a trip each semester. Last semester we visited Niagara Falls and held a full day walking photo expedition around the falls starting in the United States and crossing the border into Canada. This international expedition took an incredible amount of planning but resulted in a unique learning experience coordinating such a significant trip.</p>
-				</section>
-			</div>
-		</div><!-- row 2 -->
-	
-	<?php 
-		$person = "© Aaron Raymond 2017";
-		include "inc/footer.inc";
-		
-	?>
+			</div><!-- four -->
+			<aside>
+				<div class="five card-panel blue-grey lighten-2 z-depth-0">
+					<!-- My Extracurriculars -->
+					<section>
+						<h2 class="portfolio">Extracurriculars</h2>
+							
+							<p>I founded the UR Photography Club in 2015 to promote the learning of, creating, and sharing of the art of photography. Through my leadership of the Photo Club, I have transformed the photography community at the University of Rochester into a collaborative organization providing photography services to the campus community. In addition to professional services, Photo Club also holds a trip each semester. Last semester we visited Niagara Falls and held a full day walking photo expedition around the falls starting in the United States and crossing the border into Canada. This international expedition took an incredible amount of planning but resulted in a unique learning experience coordinating such a significant trip.</p>
+					</section>
+				</div>
+			</aside>
+
+		</div><!-- Wrapper -->
+
+		<?php 
+			$person = "© Aaron Raymond 2017";
+			include "inc/footer.inc";
+			
+		?>
 	
 	</div><!-- Container -->
 
